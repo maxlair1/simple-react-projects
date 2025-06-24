b276df6da9aa70716fc9c8594dfd18696347ceba
maxlair1 <max@lairfamily.com>
2025-06-23 22:03:48 -0400

    Fixed time formatting

--- react-stopwatch/src/Stopwatch.tsx
+++ react-stopwatch/src/Stopwatch.tsx
@@ -16,8 +16,9 @@ const Stopwatch = () => {
 
     //format time
     const hours = Math.floor(time / 360000)
-    const minutes = Math.floor((time % 360000) / 6000)
+    const minutes = Math.floor((time % 360000) / 6000);
     const seconds = Math.floor((time % 6000) / 100);
+    const milliseconds = time % 100;
 
     //start and stop the timer
     function startStopTimer() {
@@ -32,13 +33,18 @@ const Stopwatch = () => {
 
     return (
         <>
-        <div className="flex justify-center align-middle w-full h-full mx-auto">
-            <div className="flex flex-col w-full align-middle self-center items-center justify-middle">
-                <span>{hours}:{minutes}:{seconds}</span>
-                <button onClick={startStopTimer}>Start / Pause Timer</button>
-                <button onClick={resetTime}>Reset</button>
+            <div className="flex justify-center align-middle w-screen h-screen mx-auto">
+                <div className="flex flex-col w-full align-middle self-center items-center justify-middle">
+                    {/* <span>{hours}:{minutes}:{seconds}{time}</span> */}
+                    <span className="text-[50px]">
+                        {hours}:{minutes.toString().padStart(2, "0")}:
+                        {seconds.toString().padStart(2, "0")}:
+                        {milliseconds.toString().padStart(2, "0")}
+                    </span>
+                    <button onClick={startStopTimer}>Start / Pause Timer</button>
+                    <button onClick={resetTime}>Reset</button>
+                </div>
             </div>
-        </div>
         </>
     )
 }
